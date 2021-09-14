b935966e26a4d2dc0a761efa75ae534dd16ec5a5
pontusbjornf <70628477+pontusbjornf@users.noreply.github.com>
2021-09-14 14:32:36 +0200

    A
    A

--- app.js
+++ app.js
@@ -8,37 +8,123 @@ $(document).on('click','.search-cancel',function(){
 });
 
 
-const faders = document.querySelectorAll(".fade-in");
-const sliders = document.querySelectorAll(".slide-in");
+//---------------------------------------------------------------Vue cart
 
-const appearOptions = {
-  threshold: 0,
-  rootMargin: "0px 0px -250px 0px"
-};
+new Vue({
+  el: "#app",
+  data: {
+    products: [
+      {
+        image: "https://via.placeholder.com/200x150",
+        name: "Produkt",
+        description: "Beskrivning produkt",
+        price: 299,
+        quantity: 2
+      },
+      {
+        image: "https://via.placeholder.com/200x150",
+        name: "Produkt",
+        description: "Beskrivning produkt",
+        price: 446,
+        quantity: 1
+      },
+      {
+          image: "https://via.placeholder.com/200x150",
+          name: "Produkt",
+          description: "Beskrivning produkt",
+          price: 169,
+          quantity: 1
+        }
+    ],
+    tax: 5,
+    promotions: [
+      {
+        code: "SUMMER",
+        discount: "50%"
+      },
+      {
+        code: "AUTUMN",
+        discount: "40%"
+      },
+      {
+        code: "WINTER",
+        discount: "30%"
+      }
+    ],
+    promoCode: "",
+    discount: 0
+  },
+  computed: {
+    itemCount: function() {
+      var count = 0;
 
-const appearOnScroll = new IntersectionObserver(function(
-  entries,
-  appearOnScroll
-) {
-  entries.forEach(entry => {
-    if (!entry.isIntersecting) {
-      return;
-    } else {
-      entry.target.classList.add("appear");
-      appearOnScroll.unobserve(entry.target);
+      for (var i = 0; i < this.products.length; i++) {
+        count += parseInt(this.products[i].quantity) || 0;
+      }
+
+      return count;
+    },
+    subTotal: function() {
+      var subTotal = 0;
+
+      for (var i = 0; i < this.products.length; i++) {
+        subTotal += this.products[i].quantity * this.products[i].price;
+      }
+
+      return subTotal;
+    },
+    discountPrice: function() {
+      return this.subTotal * this.discount / 100;
+    },
+    totalPrice: function() {
+      return this.subTotal - this.discountPrice + this.tax;
     }
-  });
-},
-appearOptions);
+  },
+  filters: {
+    currencyFormatted: function(value) {
+      return Number(value).toLocaleString("sv", {
+        style: "currency",
+        currency: "SEK"
+      });
+    }
+  },
+  methods: {
+    updateQuantity: function(index, event) {
+      var product = this.products[index];
+      var value = event.target.value;
+      var valueInt = parseInt(value);
 
-faders.forEach(fader => {
-  appearOnScroll.observe(fader);
-});
+      // Minimum quantity is 1, maximum quantity is 100, can left blank to input easily
+      if (value === "") {
+        product.quantity = value;
+      } else if (valueInt > 0 && valueInt < 100) {
+        product.quantity = valueInt;
+      }
 
-sliders.forEach(slider => {
-  appearOnScroll.observe(slider);
-});
+      this.$set(this.products, index, product);
+    },
+    checkQuantity: function(index, event) {
+      // Update quantity to 1 if it is empty
+      if (event.target.value === "") {
+        var product = this.products[index];
+        product.quantity = 1;
+        this.$set(this.products, index, product);
+      }
+    },
+    removeItem: function(index) {
+      this.products.splice(index, 1);
+    },
+    checkPromoCode: function() {
+      for (var i = 0; i < this.promotions.length; i++) {
+        if (this.promoCode === this.promotions[i].code) {
+          this.discount = parseFloat(
+            this.promotions[i].discount.replace("%", "")
+          );
+          return;
+        }
+      }
 
-$(document).ready(()=>{
-    $("#select option[value=3]").attr('selected', 'selected');
+      alert("Sorry, the Promotional code you entered is not valid!");
+    }
+  }
 });
